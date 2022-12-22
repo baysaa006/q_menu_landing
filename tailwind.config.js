c6eb1b49c5bc73d6d189c743ce9ca61b770a8091
Enkhbayar Munkhbaysgalan <jargalgalsan@yahoo.com>
2022-12-22 07:36:47 +0100

    change

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,19 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "",
+        textColor: "#2D2D34",
+        specialText: "#C20000",
+        buttonColor: "#000",
+      },
+      fontFamily: {
+        sans: ["Rubik", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
+  },
   plugins: [],
 };
