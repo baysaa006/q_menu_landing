4f895490a8e9a236a65e1096fca957c448b0d983
Enkhbayar Munkhbaysgalan <jargalgalsan@yahoo.com>
2022-12-22 10:18:39 +0100

    change

--- component/navbar.tsx
+++ component/navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "../styles/navbar.module.css";
 import Image from "next/image";
 import logo from "../public/img/logo_with_slugin.png";
+import logo2 from "../public/img/logo.png";
 import down_arrow from "../public/icons/down_arrow.svg";
 
 export default function Navbar() {
@@ -83,54 +84,59 @@ export default function Navbar() {
           <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z" />
         </svg>
       )}
-
       {isNavExpanded && (
-        <div className={styles.expanded_menu_items}>
-          <ul className={styles.order}>
-            <li>
-              <a
-                onClick={() => {
-                  setIsNavExpanded(!isNavExpanded);
-                }}
-                href="/#feature_1"
-              >
-                <h4 className={styles.expanded_text}>Бүтээгдэхүүн</h4>
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  setIsNavExpanded(!isNavExpanded);
-                }}
-                href="/#pricing"
-              >
-                {" "}
-                <h4 className={styles.expanded_text}>Үнийн санал</h4>
-              </a>
-            </li>
-            <li>
-              <a
-                onClick={() => {
-                  setIsNavExpanded(!isNavExpanded);
-                }}
-                href="/#contact"
-              >
-                {" "}
-                <h4 className={styles.expanded_text}>Холбоо барих</h4>
-              </a>
-            </li>
-            <li>
-              <div className={styles.buttons_expanded}>
-                <button className={styles.button_sign_in}>
-                  <h4 className={styles.expanded_text_button}>Нэвтрэх</h4>
-                </button>
-                <button className={styles.button_sign_up}>
-                  <h4 className={styles.expanded_text_button}>Бүртгүүлэх</h4>
-                </button>
-              </div>
-            </li>
-          </ul>
-        </div>
+        <>
+          <div className={styles.expanded_menu_items}>
+            <ul className={styles.order}>
+              <li>
+                <a
+                  onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                  }}
+                  href="/#feature_1"
+                >
+                  <h4 className={styles.expanded_text}>Бүтээгдэхүүн</h4>
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                  }}
+                  href="/#pricing"
+                >
+                  {" "}
+                  <h4 className={styles.expanded_text}>Үнийн санал</h4>
+                </a>
+              </li>
+              <li>
+                <a
+                  onClick={() => {
+                    setIsNavExpanded(!isNavExpanded);
+                  }}
+                  href="/#contact"
+                >
+                  {" "}
+                  <h4 className={styles.expanded_text}>Холбоо барих</h4>
+                </a>
+              </li>
+              <li>
+                <div className={styles.buttons_expanded}>
+                  <button className={styles.button_sign_in}>
+                    <h4 className={styles.expanded_text_button}>Нэвтрэх</h4>
+                  </button>
+                  <button className={styles.button_sign_up}>
+                    <h4 className={styles.expanded_text_button}>Бүртгүүлэх</h4>
+                  </button>
+                </div>
+              </li>
+            </ul>
+            <div className={styles.footer}>
+              {" "}
+              <h4 className={styles.expanded_text}>© 2022 Q-Menu</h4>
+            </div>
+          </div>
+        </>
       )}
     </div>
   );
